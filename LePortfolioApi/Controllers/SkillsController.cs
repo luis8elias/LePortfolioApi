e1661a9d5058fee4a007df261115001ceecbd81c
Luis Elias <luis8alfredo.elias@gmail.com>
2023-05-02 18:32:01 -0700

    add skills controller

---
+++ LePortfolioApi/Controllers/SkillsController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LePortfolioApi.Data;
+using LePortfolioApi.Models;
+using LePortfolioApi.Util;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+
+namespace LePortfolioApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SkillsController : ControllerBase
+    {
+        private readonly EfContext _context;
+
+        public SkillsController(EfContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Skills
+        [HttpGet("/Skills")]
+        [ProducesResponseType(typeof(BasicResponse<IEnumerable<Skill>>), 200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public async Task<ActionResult<BasicResponse<IEnumerable<Skill>>>> GetSkills()
+        {
+         
+
+            try
+            {
+
+                var skills = await _context.Skills.ToListAsync();
+
+                if (skills == null)
+                {
+                    return NotFound(ResponseManager.NotFound("Sin Skills"));
+                }
+               
+
+                return Ok(ResponseManager.OK("Success", skills));
+
+
+            }
+            catch (Exception e)
+            {
+                
+                return BadRequest(ResponseManager.Error(e));
+                throw;
+            }
+
+           
+        }
+
+    }
+}
---
+++ LePortfolioApi/Util/ResponseManager.cs
@@ -0,0 +1,41 @@
+﻿namespace LePortfolioApi.Util
+{
+    public class ResponseManager
+    {
+        public static BasicResponse Error(Exception ex)
+        {
+            return new BasicResponse
+            {
+                Message = ex.Message,
+                Success = false,
+            };
+        }
+        public static BasicResponse BadRequest(string exception)
+        {
+            return new BasicResponse
+            {
+                Message = exception,
+                Success = false,
+            };
+        }
+
+        public static BasicResponse<object> OK(string message, object? response)
+        {
+            return new BasicResponse<object>
+            {
+                Message = message,
+                Success = true,
+                Model = response
+            };
+        }
+
+        public static BasicResponse NotFound(string message)
+        {
+            return new BasicResponse
+            {
+                Message = message,
+                Success = false,
+            };
+        }
+    }
+}
